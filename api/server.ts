4c03dff9c696b829d1d36bd02cdf0abbde5d75f4
Sourabh Ray <roysourabh9881@gmail.com>
2026-05-19 00:42:43 +0530

    vercel fix

--- api/server.ts
+++ api/server.ts
@@ -10,10 +10,6 @@ type ServerEntry = {
 
 const server = serverEntry as ServerEntry;
 
-export const config = {
-  runtime: "nodejs22.x",
-};
-
 export default async function handler(request: Request): Promise<Response> {
   return server.fetch(request, process.env, {});
 }
